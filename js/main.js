05ecaec1e33b394b92c51494603e7a4bdde27020
Livia Silva <38299821+livribeiro@users.noreply.github.com>
2020-06-25 22:10:43 -0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function startApp() {
+
+    let button = document.getElementById('btnlogin');
+    button.onclick = function() {
+        verifica();
+    };
+}
+
+function verfica() {
+    let id = document.getElementById("idaluno").value
+    var senha = document.getElementById("senha").value
+
+    if (id == "") {
+        let paragraph = document.getElementsByClassName('result1');
+        paragraph.innerHTML = 'Senha/Password invalidos ';
+    }
+    if (pass == "") {
+        let paragraph = document.getElementsByClassName('result2');
+        paragraph.innerHTML = 'Senha/Password invalidos ';
+    }
+    if (id || senha == "") {
+        let paragraph = document.getElementsByClassName('result2');
+        paragraph.innerHTML = 'Senha/Password invalidos';
+
+    } else {
+        alert("Seja Bem vindo!")
+    }
+
+
+}
